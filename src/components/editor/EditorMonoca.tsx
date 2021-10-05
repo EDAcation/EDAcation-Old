fac7f212c649226c5c84ab66a745e1dc15a0ccd7
Daniel Huisman <daniel@huisman.me>
2021-10-05 16:50:32 +0200

    Change react-monaco-editor to @monaco-editor/react

--- src/components/editor/EditorMonoca.tsx
+++ src/components/editor/EditorMonoca.tsx
@@ -1,25 +1,16 @@
 import React, {useState} from 'react';
-import MonacoEditor, {EditorDidMount} from 'react-monaco-editor';
+import MonacoEditor from '@monaco-editor/react';
 
 export const EditorMonaco: React.FC = () => {
     const [value, setValue] = useState('');
 
-    const handleMount: EditorDidMount = (editor) => {
-        editor.focus();
-    };
-
-    // TODO: Requires language loading, see https://github.com/Microsoft/monaco-editor-webpack-plugin.
-    //       Parcel v2 does not use Webpack, so this needs to be loaded otherwise.
-
     return (
         <MonacoEditor
-            width="100%"
-            height="100%"
             language="json"
             theme="vs-dark"
             value={value}
-            onChange={(newValue) => setValue(newValue)}
-            editorDidMount={handleMount}
+            onChange={(newValue) => setValue(newValue || value)}
+            onMount={(editor) => editor.focus()}
         />
     );
 };
