1d0257a1f7badf3f0fea4f01fcf9b517d38c667b
Daniel Huisman <daniel@huisman.me>
2022-04-06 12:33:42 +0200

    Add error handling to DigitalJS editor

--- src/components/editor/EditorDigitalJS.tsx
+++ src/components/editor/EditorDigitalJS.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {yosys2digitaljs} from 'yosys2digitaljs';
 import {Circuit} from 'digitaljs';
 
@@ -8,19 +8,36 @@ export type EditorDigitalJSProps = BaseEditorProps;
 
 export const EditorDigitalJS: React.FC<EditorDigitalJSProps> = ({panelId, file}) => {
     const ref = useRef<HTMLDivElement>(null);
+    const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
-        const json = JSON.parse(file.content);
-        console.log(json);
-        const digitalJs = yosys2digitaljs(json);
-        console.log(digitalJs);
-        const circuit = new Circuit(digitalJs);
-        console.log(circuit);
-        const paper = circuit.displayOn(ref.current);
-        console.log(paper);
+        try {
+            const json = JSON.parse(file.content);
+            console.log(json);
+            const digitalJs = yosys2digitaljs(json);
+            console.log(digitalJs);
+            const circuit = new Circuit(digitalJs);
+            console.log(circuit);
+            const paper = circuit.displayOn(ref.current);
+            console.log(paper);
+        } catch (err) {
+            console.error(err);
+
+            if (err instanceof Error) {
+                setError(err);
+            }
+        }
     }, [panelId, file.id, file.content]);
 
     return (
-        <div id={`digitaljs-${panelId}-${file.id}`} ref={ref} />
+        <>
+            <div id={`digitaljs-${panelId}-${file.id}`} ref={ref} />
+
+            {error && (
+                <div style={{padding: '0.5rem 1rem'}}>
+                    <code>{error.stack || error.toString()}</code>
+                </div>
+            )}
+        </>
     );
 };
