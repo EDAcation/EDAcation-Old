273d7770c43af03e88fb697726126ddb6da06795
Daan Middelkoop <daantje676@gmail.com>
2022-04-20 10:51:08 +0200

    Add buttons to digitaljs simulation

--- src/components/editor/EditorDigitalJS.tsx
+++ src/components/editor/EditorDigitalJS.tsx
@@ -8,19 +8,26 @@ export type EditorDigitalJSProps = BaseEditorProps;
 
 export const EditorDigitalJS: React.FC<EditorDigitalJSProps> = ({panelId, file}) => {
     const ref = useRef<HTMLDivElement>(null);
+    let circuit: Circuit;
 
     useEffect(() => {
         const json = JSON.parse(file.content);
         console.log(json);
         const digitalJs = yosys2digitaljs(json);
         console.log(digitalJs);
-        const circuit = new Circuit(digitalJs);
-        console.log(circuit);
+        circuit = new Circuit(digitalJs);
+        circuit.start();
         const paper = circuit.displayOn(ref.current);
         console.log(paper);
+
     }, [panelId, file.id, file.content]);
 
     return (
-        <div id={`digitaljs-${panelId}-${file.id}`} ref={ref} />
+        <div>
+            <button onClick={() => circuit?.start()}>Start</button>
+            <button onClick={() => circuit?.stop()}>Stop</button>
+            <button onClick={() => circuit?.updateGates()}>Step</button>
+            <div id={`digitaljs-${panelId}-${file.id}`} ref={ref} />
+        </div>
     );
 };
