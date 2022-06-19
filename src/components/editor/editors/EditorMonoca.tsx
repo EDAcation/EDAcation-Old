06fe51fa3095c43d22d0d49f0808ae116f7b472e
Daniel Huisman <daniel@huisman.me>
2022-06-20 01:20:05 +0200

    Fix editor saving

--- src/components/editor/editors/EditorMonoca.tsx
+++ src/components/editor/editors/EditorMonoca.tsx
@@ -1,5 +1,5 @@
-import React, {useCallback} from 'react';
-import MonacoEditor, {loader, OnMount} from '@monaco-editor/react';
+import React, {useCallback, useEffect} from 'react';
+import MonacoEditor, {loader} from '@monaco-editor/react';
 
 import {useAppSelector} from '../../../store';
 import {EditorFileLoaded} from '../../../store/files';
@@ -20,24 +20,46 @@ export const EditorMonaco: React.FC<EditorMonacoProps> = ({panelId, file, value,
     // NOTE: Ugly hack to have file access in commands
     latestFile = file;
 
-    const handleMount: OnMount = useCallback(
-        (editor, monaco) => {
-            // @ts-expect-error: KEY_S does not exist on the type, but gives the correct value
-            editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
+    const onKeyDown = useCallback(
+        (event: KeyboardEvent) => {
+            if ((event.ctrlKey || event.metaKey) && event.key === 's') {
+                event.preventDefault();
+
                 if (onSave) {
                     onSave(latestFile);
                 }
-            });
+            }
         },
         [onSave]
     );
+
+    useEffect(() => {
+        document.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown);
+        };
+    }, [onKeyDown]);
+
+    // const handleMount: OnMount = useCallback(
+    //     (editor, monaco) => {
+    //         // @ts-expect-error: KEY_S does not exist on the type, but gives the correct value
+    //         editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, (...args) => {
+    //             console.log(...args);
+    //             if (onSave) {
+    //                 onSave(latestFile);
+    //             }
+    //         });
+    //     },
+    //     [onSave]
+    // );
 
     return (
         <MonacoEditor
             path={`${panelId}/${file.file.getFullPath()}`}
             theme={theme === 'light' ? 'vs-light' : 'vs-dark'}
             value={value}
-            onMount={handleMount}
+            // onMount={handleMount}
             onChange={(newValue) => onChange(file, newValue ?? '')}
             options={{
                 automaticLayout: true
